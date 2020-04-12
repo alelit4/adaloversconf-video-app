90b6e8bd71f0c6dc1efdf22cab9d154fa3bb5a5a
Alelit4 <alelit4@gmail.com>
2020-04-12 12:54:05 +0100

    typewriter effect

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,17 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import Typewriter from 'typewriter-effect/dist/core';
 
 class Loading extends Component {
 
+    componentDidMount(){
+        this.typewriter = new Typewriter(this.$loading, 
+            {strings : [this.props.message],
+                autoStart: true});
+    }
+   
     render(){
-        return (<div className="loading" > {this.props.message || 'Loading...'} </div>);
+        return (<div className="loading" ref={el => this.$loading = el}> </div>);
     }
 
 }
